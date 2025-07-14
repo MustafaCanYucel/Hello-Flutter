e3329f87c20413fad1eceaf7ace3642056320c9a
Mustafa Can Yucel <mustafacanyucel01@gmail.com>
2025-07-14 10:17:03 +0300

    Add lesson 04: AppBar with custom styling

---
+++ 04-appbar-custom-style/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Uygulamam());
+}
+
+class Uygulamam extends StatelessWidget {
+  const Uygulamam({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "Nunito"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Uygulamam',
+              style:
+                  TextStyle(fontWeight: FontWeight.w300, color: Colors.white)),
+          backgroundColor: Colors.indigo.shade200,
+          centerTitle: true,
+        ),
+        body: Text('merhaba'),
+      ),
+    );
+  }
+}
